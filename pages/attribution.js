636d4663efb536e12143bd96d42ff13423198274
Jérôme Muffat-Méridol <jmuffat@webphotomag.com>
2020-12-07 15:22:45 +0100

    map attribution

--- pages/attribution.js
+++ pages/attribution.js
@@ -8,6 +8,7 @@ function Page() {
     <BasePage>
       <h1>Attribution </h1>
       <p>Some icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik" target="_blank">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon" target="_blank">www.flaticon.com</a></p>
+      <p>Made with Natural Earth. Free vector and raster map data @ <a href="https://www.naturalearthdata.com/" target="_blank">naturalearthdata.com</a>.</p>
     </BasePage>
   )
 }
