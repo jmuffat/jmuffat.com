b231a1f2393ab62e9a782d5a650a8d0d028f13d9
Jérôme Muffat-Méridol <jmuffat@webphotomag.com>
2021-01-26 16:43:39 +0100

    simplify tracking of what needs translating to french

--- i18n/todo-fr.js
+++ i18n/todo-fr.js
@@ -1,15 +1,55 @@
 const fs = require('fs')
 const path = require('path')
 
+const {extract} = require('@formatjs/cli')
+
 async function loadJson(a) {
   const filepath = path.join(__dirname,a)
   const res = await fs.promises.readFile(filepath,{encoding:'utf-8'})
   return JSON.parse(res)
 }
 
+const reIsJS = /\.js$/
+
+async function walk(dir) {
+  var results = [];
+  const list = await fs.promises.readdir(dir,{withFileTypes:true})
+
+  const files = (
+    list
+    .filter(dirent=> reIsJS.test(dirent.name) && !dirent.isDirectory() )
+    .map(dirent=>path.resolve(dir,dirent.name))
+  )
+
+  const foldersFiles = await Promise.all(
+    list
+    .filter(dirent=> dirent.isDirectory())
+    .map(dirent=>walk(path.resolve(dir,dirent.name)))
+  )
+
+  const flatFoldersFiles = foldersFiles.reduce((cur,a)=>cur.concat(a),[])
+
+  return files.concat(flatFoldersFiles);
+};
+
+async function extractMessages() {
+  const pages      = await walk(path.resolve(__dirname,'..','pages'))
+  const components = await walk(path.resolve(__dirname,'..','components'))
+  const fileset    = pages.concat(components)
+
+  const resultAsString = await extract(
+    fileset,
+    {
+      idInterpolationPattern: '[sha512:contenthash:base64:6]'
+    }
+  )
+
+  return JSON.parse(resultAsString)
+}
+
 async function work() {
+  const needed  = await extractMessages()
   const current = await loadJson('fr-FR/lang.json')
-  const needed  = await loadJson('lang.json')
 
   const toAdd = Object.keys(needed).reduce(
     (cur,a)=>{
