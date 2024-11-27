08dd3d83bbddfbc0c5016a382ef699b3978c861b
Jérôme Muffat-Méridol <jmuffat@webphotomag.com>
2024-11-27 14:19:37 +0100

    fix locale selector

--- app/[lang]/i18n.js
+++ app/[lang]/i18n.js
@@ -2,7 +2,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import {IntlProvider} from 'react-intl'
+import {IntlProvider, useIntl} from 'react-intl'
 import {cn} from '@/components/cn'
 
 import FlagFR from './fr.svg'
@@ -11,6 +11,7 @@ import FlagEN from './en.svg'
 export const LocalizedMessagesProvider = (props)=> <IntlProvider {...props}/>
 
 export function LocaleSelector({available = ['en','fr']}) {
+	const intl = useIntl()
 	const pathname = usePathname()
 	const locales = [
 		{id:'en', img: FlagEN},
@@ -24,10 +25,10 @@ export function LocaleSelector({available = ['en','fr']}) {
 		<ul className="max-w-5xl mx-auto px-4 py-1 gap-2 flex flex-row justify-end place-items-center border-r border-l border-secondary">
 			{locales.map(loc => {
 				const className = cn({
-					'h-4 w-6 border rounded relative': true,
-					'border-red': loc.id === locale,
-					'border-transparent': loc.id !== locale,
-					'blur-[1px] opacity-50': !available.find(a=>a.id==loc.id)
+					'h-4 w-6 border relative': true,
+					'border-red-400 bg-red-400': loc.id === intl.locale,
+					'border-transparent': loc.id !== intl.locale,
+					'blur-[1px] opacity-50': !available.includes(loc.id)
 				})
 
 				return (
