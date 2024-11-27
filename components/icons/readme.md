6efa604b7494e5b8a1b270f6b92ed9bdda964f82
Jérôme Muffat-Méridol <jmuffat@webphotomag.com>
2024-11-27 16:27:38 +0100

    style sharing button

---
+++ components/icons/readme.md
@@ -0,0 +1,3 @@
+# method to get icons
+- downloaded .svg from [simpleicons](https://simpleicons.org/)
+- convert to .js using [react-svgr](https://react-svgr.com/playground/?icon=true)
--- components/share-button.js
+++ components/share-button.js
@@ -1,43 +1,43 @@
 "use client"
 import React from 'react'
+import { Share } from 'lucide-react'
+import FacebookIcon from "./icons/facebook"
+import { Button } from "@/components/ui/button"
+
 
 function NativeShare(props) {
 
-  const onPress = ()=>{
-    navigator.share({
-      title: props.title || 'jmuffat.com',
-      text: props.text,
-      url: props.url,
-    })
-  }
+	const onPress = () => {
+		navigator.share({
+			title: props.title || 'jmuffat.com',
+			text: props.text,
+			url: props.url,
+		})
+	}
 
-  return (
-    <button onClick={onPress}><i className="fas fa-share-alt"></i> share</button>
-  );
+	return (
+		<Button variant="outline" onClick={onPress}><Share /> share</Button>
+	);
 }
 
-
 function ButtonBar(props) {
-  const onPress = ()=>{
-    window.location.href = `https://www.facebook.com/sharer/sharer.php?u=${props.url}`;
-  }
+	const onPress = () => {
+		window.location.href = `https://www.facebook.com/sharer/sharer.php?u=${props.url}`;
+	}
 
-  return (
-    <button onClick={onPress}>
-      <i className="fab fa-facebook" style={{color:"#1877f2"}}></i> share
-    </button>);
+	return <Button variant="outline" onClick={onPress}><FacebookIcon/> share</Button>
 }
 
 export default function ShareButton(props) {
-  var [shareMethod,setShareMethod] = React.useState(0)
+	var [shareMethod, setShareMethod] = React.useState(0)
 
-  React.useEffect(()=>{
-    setShareMethod(navigator.share? 1 : 2)
-  },[])
+	React.useEffect(() => {
+		setShareMethod(navigator.share? 1 : 2)
+	}, [])
 
-  switch (shareMethod) {
-      case 0: return null;
-      case 1: return <NativeShare {...props}/>;
-      case 2: return <ButtonBar {...props}/>;
-  }
+	switch (shareMethod) {
+		case 0: return null;
+		case 1: return <NativeShare {...props} />;
+		case 2: return <ButtonBar {...props} />;
+	}
 }
