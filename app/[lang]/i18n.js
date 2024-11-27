2f85b8975edf867a64632dde75ab516554599c38
Jérôme Muffat-Méridol <jmuffat@webphotomag.com>
2024-11-27 10:57:54 +0100

    keep flag images with the component using them

--- app/[lang]/i18n.js
+++ app/[lang]/i18n.js
@@ -5,30 +5,35 @@ import { usePathname } from 'next/navigation'
 import {IntlProvider} from 'react-intl'
 import {cn} from '@/components/cn'
 
+import FlagFR from './fr.svg'
+import FlagEN from './en.svg'
 
 export const LocalizedMessagesProvider = (props)=> <IntlProvider {...props}/>
 
-export function LocaleSelector({locale}) {
+export function LocaleSelector({available = ['en','fr']}) {
 	const pathname = usePathname()
-	const locales = ['en','fr']
+	const locales = [
+		{id:'en', img: FlagEN},
+		{id:'fr', img: FlagFR}
+	]
 	
 	const re = /^\/[^\/]*(\/.*)/.exec(pathname)
 	const basePath = re? re[1] : "/"
   
 	return (
-		<ul className="max-w-5xl mx-auto px-4 py-1 flex flex-row justify-end place-items-center border-r border-l border-secondary">
+		<ul className="max-w-5xl mx-auto px-4 py-1 gap-2 flex flex-row justify-end place-items-center border-r border-l border-secondary">
 			{locales.map(loc => {
 				const className = cn({
-					'pl-1 border rounded': true,
-					'border-red': loc === locale,
-					'border-transparent': loc !== locale,
-					'blur-[1px] opacity-50': !locales.includes(loc)
+					'h-4 w-6 border rounded relative': true,
+					'border-red': loc.id === locale,
+					'border-transparent': loc.id !== locale,
+					'blur-[1px] opacity-50': !available.find(a=>a.id==loc.id)
 				})
 
 				return (
-					<li key={loc} className={className}>
-						<Link href={`/${loc}${basePath}`}>
-							<Image src={`/img/${loc}.svg`} alt={loc} width="30" height="20"/>
+					<li key={loc.id} className={className}>
+						<Link href={`/${loc.id}${basePath}`}>
+							<Image src={loc.img} alt={loc.id} fill/>
 						</Link>
 					</li>
 				)
