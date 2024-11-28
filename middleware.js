6b23b10ff13285baa9554fc228ca61e59e9db5c0
Jérôme Muffat-Méridol <jmuffat@webphotomag.com>
2024-11-28 17:58:31 +0100

    make our custom i18n handling work

--- middleware.js
+++ middleware.js
@@ -6,69 +6,58 @@ const locales = ['en', 'fr']
 const defaultLocale = locales[0]
 
 const localeReady = new Set([
-  ...locales,
-  'download',
-  'img',
-
-  'android-icon-36x36.png',
-  'android-icon-48x48.png',
-  'android-icon-72x72.png',
-  'android-icon-96x96.png',
-  'android-icon-144x144.png',
-  'android-icon-192x192.png',
-  'apple-icon-57x57.png',
-  'apple-icon-60x60.png',
-  'apple-icon-72x72.png',
-  'apple-icon-76x76.png',
-  'apple-icon-114x114.png',
-  'apple-icon-120x120.png',
-  'apple-icon-144x144.png',
-  'apple-icon-152x152.png',
-  'apple-icon-180x180.png',
-  'apple-icon-precomposed.png',
-  'apple-icon.png',
-  'browserconfig.xml',
-  'favicon-16x16.png',
-  'favicon-32x32.png',
-  'favicon-96x96.png',
-  'favicon.ico',
-  'manifest.json',
-  'ms-icon-70x70.png',
-  'ms-icon-144x144.png',
-  'ms-icon-150x150.png',
-  'ms-icon-310x310.png',
+	...locales,
+	'download',
+	'img'
 ])
 
 // Get the preferred locale, similar to the above or using a library
 function getLocale(request) {
-    const h = 'accept-language'
-    const headers = {[h]: request.headers.get(h)}
-    const nego = new Negotiator({headers})
-    const languages = nego.languages()
-    return match(languages, locales, defaultLocale)
+	const h = 'accept-language'
+	const headers = { [h]: request.headers.get(h) }
+	const nego = new Negotiator({ headers })
+	const languages = nego.languages()
+	return match(languages, locales, defaultLocale)
 }
- 
+
 export function middleware(request) {
-  // Check if there is any supported locale in the pathname
-  const { pathname } = request.nextUrl
-  
-  const re = /^\/([^\/?#]*)/.exec(pathname)
-  if (!re) return
+	// Check if there is any supported locale in the pathname
+	const { pathname } = request.nextUrl
 
+	const re = /^\/([^\/?#]*)/.exec(pathname)
+	if (!re) return
 
-  if (!localeReady.has(re[1])) {
-    // Redirect if there is no locale
-    const locale = getLocale(request)
-    request.nextUrl.pathname = `/${locale}${pathname}`
-    // e.g. incoming request is /products
-    // The new URL is now /en-US/products
-    return NextResponse.redirect(request.nextUrl)
-  }
+	const restOfPath = pathname.substring(re[1].length+1)
+
+	if (localeReady.has(re[1])) return
+
+	// We need to redirect
+	switch (re[1].toLowerCase()) {
+		/* cases where we want to rename the locale, so old url's still work */ 
+		case 'fr-fr': {
+			request.nextUrl.pathname = `/fr${restOfPath}`
+		}	break
+
+		case 'en-us':
+		case 'en-gb': {
+			request.nextUrl.pathname = `/en${restOfPath}`
+		}	break
+
+		/* cases where the locale wasn't specified and we need to _add_ it */ 
+		default: {
+			const locale = getLocale(request)
+			request.nextUrl.pathname = `/${locale}${pathname}`
+		}
+	}
+
+	return NextResponse.redirect(request.nextUrl)
 }
- 
+
+// localhost:3000/fr-FR/posts/20210201-i18n
+
 export const config = {
-  matcher: [
-    '/((?!_next).*)',
-    '/'
-  ],
+	matcher: [
+		'/((?!_next).*)',
+		'/'
+	],
 }
--- next.config.mjs
+++ next.config.mjs
@@ -5,31 +5,31 @@ import createMDX from '@next/mdx'
 import remarkGfm from 'remark-gfm'
 import rehypeExternalLinks from 'rehype-external-links'
 
-import i18nConfig from './i18n-config.mjs'
+// import i18nConfig from './i18n-config.mjs'
 
 async function redirects() {
-  // old blog used this format : /2020/03/04/my-first-paid-job
-  return [
-    {
-      source: '/:yy(\\d{1,})/:mm(\\d{1,})/:dd(\\d{1,})/:slug',
-      destination: '/posts/:yy:mm:dd-:slug',
-      permanent: true
-    }
-  ]
+	// old blog used this format : /2020/03/04/my-first-paid-job
+	return [
+		{
+			source: '/:yy(\\d{1,})/:mm(\\d{1,})/:dd(\\d{1,})/:slug',
+			destination: '/posts/:yy:mm:dd-:slug',
+			permanent: true
+		}
+	]
 }
 
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  i18n: i18nConfig.nextjsConfig,
-  redirects,
+	pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+	// i18n: i18nConfig.nextjsConfig,
+	redirects,
 
-  sassOptions: {
-    silenceDeprecations: ['legacy-js-api'],
-  },
-  
-  env: {
-    ROOT: path.resolve(process.cwd())
-  },
+	sassOptions: {
+		silenceDeprecations: ['legacy-js-api'],
+	},
+
+	env: {
+		ROOT: path.resolve(process.cwd())
+	},
 }
 
 const withMDX = createMDX({
@@ -39,7 +39,7 @@ const withMDX = createMDX({
 		],
 		rehypePlugins: [
 			[rehypeExternalLinks, { target: '_blank' }],
-		],	
+		],
 	}
 })
 
