a99d8310d28983ed54335a35f1f7a423bf22e1f3
Jérôme Muffat-Méridol <jmuffat@webphotomag.com>
2024-12-09 16:07:15 +0100

    preprocess contents

---
+++ content-idx/gen.mjs
@@ -0,0 +1,62 @@
+import path from 'path'
+import fsp from 'fs/promises'
+import matter from 'gray-matter'
+
+async function parseFolder(
+    index,
+    folder,
+    sitepath
+)
+{
+    const rePage = /^page\.(js|jsx|md|mdx|ts|tsx)$/
+    const reContent = /^content(?:\.(.*))?.mdx$/
+    const entries = await fsp.readdir(folder,{withFileTypes:true})
+
+    // parse folder
+    const page = {}
+    for(const e of entries) {
+        const currentPath = ()=> path.join(folder, e.name)
+
+        if (!e.isFile()) continue
+
+        if (rePage.test(e.name)) {
+            page.src = currentPath()
+            page.sitepath = sitepath
+            continue
+        }
+
+        const content = reContent.exec(e.name)
+        if (content) {
+            const locale = content[1] ?? '0'
+            const fileContents = await fsp.readFile(currentPath(),{encoding:'utf8'})
+            const {data} = matter(fileContents)
+
+            page[locale] = data
+            continue
+        }
+    }
+
+    if (page.src) index.push(page)
+
+    // recurse
+    for(const e of entries) {
+        if (!e.isDirectory()) continue
+
+        const subfolder = path.join(folder, e.name)
+        const subpath   = `${sitepath}/${e.name}`
+        await parseFolder(index, subfolder,subpath)
+    }
+}
+
+async function run() {
+    const appRoot = path.join(process.cwd(), 'app', '[lang]')
+    const index = []
+
+    await parseFolder(index,appRoot,'')
+
+    const destPath = path.join(process.cwd(), 'content-idx', 'index.json')
+    const data = JSON.stringify(index, null, 2)
+    await fsp.writeFile(destPath, data, {encoding:'utf8'})
+}
+
+run()
