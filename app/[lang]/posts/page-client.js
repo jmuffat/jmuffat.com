580ef7cbde9a131159976f30b26f0bd32e090360
Jérôme Muffat-Méridol <jmuffat@webphotomag.com>
2024-12-19 12:06:47 +0100

    improve dates in posts list

--- app/[lang]/posts/page-client.js
+++ app/[lang]/posts/page-client.js
@@ -4,8 +4,8 @@ import {FormattedDate} from 'react-intl'
 export const ThreadDate = ({children})=>{
     const value = new Date(children)
     return (
-        <span className='inline-block w-20'>
-            <FormattedDate value={value} year="numeric" month="2-digit" day="2-digit"/>
+        <span className='inline-block text-xs font-thin w-20'>
+            <FormattedDate value={value} year="numeric" month="short" day="2-digit"/>
         </span>
     )
 }
