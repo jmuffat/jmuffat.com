b114ea268014870061e5400961bb7506b1f51fae
Jérôme Muffat-Méridol <jmuffat@webphotomag.com>
2025-01-06 15:14:29 +0100

    post about duplication

---
+++ app/[lang]/posts/20250106-duplication/page.js
@@ -0,0 +1,18 @@
+import {genPostPage,PostPageMetadata} from '@/components/post'
+import Content, {matter} from './content.en.mdx'
+// import ContentEN, {matter as matterEN} from './content.en.mdx'
+// import ContentFR, {matter as matterFR} from './content.fr.mdx'
+import cover from './opengraph-image.webp'
+
+const postdata = {
+	matter, Content, 
+	// matterEN, ContentEN,
+	// matterFR, ContentFR,
+	cover,
+
+	src: import.meta.url,
+}
+
+export const generateMetadata = PostPageMetadata(postdata)
+const Page = genPostPage(postdata)
+export default Page
